cc14eafefc3225236755160e99a941e45f78d6e1
Anderly Teleguario <anderlyteleguario@outlook.com>
2025-10-02 22:07:33 -0600

    Producto talla color en packing

---
+++ TOMIMSV4/Entity/Transacciones/Packing/clsBeTrans_packing_enc_Partial.vb
@@ -0,0 +1,34 @@
+﻿Partial Public Class clsBeTrans_packing_enc
+    Implements IDisposable
+
+    Private disposedValue As Boolean
+    Public Property Nombre_Talla As String = ""
+    Public Property Codigo_Talla As String = ""
+    Public Property Nombre_Color As String = ""
+    Public Property Codigo_Color As String = ""
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: eliminar el estado administrado (objetos administrados)
+            End If
+
+            ' TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+            ' TODO: establecer los campos grandes como NULL
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: reemplazar el finalizador solo si "Dispose(disposing As Boolean)" tiene código para liberar los recursos no administrados
+    ' Protected Overrides Sub Finalize()
+    '     ' No cambie este código. Coloque el código de limpieza en el método "Dispose(disposing As Boolean)".
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' No cambie este código. Coloque el código de limpieza en el método "Dispose(disposing As Boolean)".
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
