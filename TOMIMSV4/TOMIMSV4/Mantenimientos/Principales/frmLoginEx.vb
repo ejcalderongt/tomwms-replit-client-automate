afab7cb66a61ea588b75b6fad37b8d1a8c80caf9
Marcela1306 <agmarcela1306@gmail.com>
2026-02-13 08:48:46 -0600

    actualizar

--- TOMIMSV4/TOMIMSV4/Mantenimientos/Principales/frmLoginEx.vb
+++ TOMIMSV4/TOMIMSV4/Mantenimientos/Principales/frmLoginEx.vb
@@ -690,6 +690,7 @@ Public Class frmLoginEx
             lLicenciasProgramacion.Add("DESKTOP-790O7S2")
             lLicenciasProgramacion.Add("DESKTOP-5BM5P11")
             lLicenciasProgramacion.Add("DESKTOP-790O7S2")
+            lLicenciasProgramacion.Add("Marcela1306")
 
             '#EJC20210517: Cuando lo encuentren, 
             'agreguen sus Identificadores en la lista para que en caso de que no tenga licencia, no les pida.
