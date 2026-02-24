7c2d460ea8c833abe276c6f9d795dab178be3a4b
erickc <erickc@wmsmampa>
2026-02-24 17:08:15 -0600

    Enviando cambios y mejoras

--- TOMIMSV4/TOMIMSV4/Clases_AP/m_Global.vb
+++ TOMIMSV4/TOMIMSV4/Clases_AP/m_Global.vb
@@ -19,10 +19,10 @@ Module m_Global
     Public vRutaServicio As String = CurDir() & "/WMS_PrintService.exe"
     Public vRutaInterfaceCEALSA As String = CurDir() & "/CEALSASync.exe"
 
-    Public Property gVersionApp As String = "7.9.6"
+    Public Property gVersionApp As String = "7.9.9"
 
     Public gVersionBD As String = "1"
-    Public Property gFechaVersion As Date = New Date(2026, 2, 20)
+    Public Property gFechaVersion As Date = New Date(2026, 2, 24)
     Public Property wsTOMHHInstance As TOMHHWSSoapClient
 
     Public gIndiceInstancia As Integer = -1
