5338c302da461085abc7bd72052b3dd1b75b028d
Carolina Fuentes <carolinakfk@hotmail.com>
2026-05-05 13:24:01 -0600

    Correcciones y campos nuevos

--- TOMIMSV4/Entity/Transacciones/Pedido/TipoPedido/clsBeTrans_pe_tipo.vb
+++ TOMIMSV4/Entity/Transacciones/Pedido/TipoPedido/clsBeTrans_pe_tipo.vb
@@ -82,6 +82,8 @@ Public Class clsBeTrans_pe_tipo
     Public Property Verificar_con_imagen As Boolean = False
     Public Property Genera_Guia_Remision As Boolean = False
     Public Property Asignar_Todos_Operadores As Boolean = False
+    Public Property Generar_Picking_Auto As Boolean = False
+
 
     Sub New()
     End Sub
