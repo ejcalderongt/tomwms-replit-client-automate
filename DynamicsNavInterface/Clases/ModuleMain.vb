2c3ded7855e49e9317fdba0e47cc34dd2b3d2918
Erik José Calderón <yejc27@hotmail.com>
2026-05-19 16:57:16 -0600

    set debug on, cuidado!!!!

--- DynamicsNavInterface/Clases/ModuleMain.vb
+++ DynamicsNavInterface/Clases/ModuleMain.vb
@@ -147,6 +147,8 @@ Module ModuleMain
                             BD.Instancia.IdConfiguracionInterface = IdConfiguracion
                         End If
 
+                        BD.Instancia.Modo_Debug = True
+
                         Configuration.ConfigurationManager.AppSettings("CST") = BD.Instancia.CadenaConexionSQLClient
                         Configuration.ConfigurationManager.AppSettings("WMS_MODO_DEBUG") = IIf(BD.Instancia.Modo_Debug, "ON", "OFF")
                         Configuration.ConfigurationManager.AppSettings("WMS_RESERVA_MI3_TRACE") = IIf(BD.Instancia.Modo_Debug, "ON", "OFF")
