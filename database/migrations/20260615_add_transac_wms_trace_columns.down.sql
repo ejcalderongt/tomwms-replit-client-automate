5d7e5f14675f32e8481c85cd1e33d878323bc2d2
ejcalderongt <ejcalderon02@yahoo.com>
2026-06-15 18:02:53 -0600

    add transac wms trace columns

---
+++ database/migrations/20260615_add_transac_wms_trace_columns.down.sql
@@ -0,0 +1,32 @@
+﻿SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+BEGIN TRY
+    BEGIN TRAN;
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_det_error', 'human_error') IS NOT NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_det_error
+            DROP COLUMN human_error;
+    END
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_det_error', 'level_log') IS NOT NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_det_error
+            DROP COLUMN level_log;
+    END
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_res', 'tipo_documento_sap') IS NOT NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_res
+            DROP COLUMN tipo_documento_sap;
+    END
+
+    COMMIT TRAN;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRAN;
+
+    THROW;
+END CATCH;
---
+++ database/migrations/20260615_add_transac_wms_trace_columns.up.sql
@@ -0,0 +1,32 @@
+﻿SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+BEGIN TRY
+    BEGIN TRAN;
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_res', 'tipo_documento_sap') IS NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_res
+            ADD tipo_documento_sap NVARCHAR(50) NULL;
+    END
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_det_error', 'level_log') IS NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_det_error
+            ADD level_log NVARCHAR(25) NULL;
+    END
+
+    IF COL_LENGTH('dbo.i_nav_ejecucion_det_error', 'human_error') IS NULL
+    BEGIN
+        ALTER TABLE dbo.i_nav_ejecucion_det_error
+            ADD human_error NVARCHAR(1000) NULL;
+    END
+
+    COMMIT TRAN;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRAN;
+
+    THROW;
+END CATCH;
